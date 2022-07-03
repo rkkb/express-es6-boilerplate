cef71f22e1b502843b17a8fa857d22981e216703
rkkb <rkkb7828131@gmail.com>
2022-07-03 17:40:10 +0530

    boilerplate

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express, { json } from "express";
+import items from "./items";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", async (req, res) => {
+  res.json({ status: true, message: "Our node.js app works" });
+});
+
+app.get("/items", (req, res) => {
+  res.json({ status: true, message: "Fetched all items", data: items });
+});
+
+app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
